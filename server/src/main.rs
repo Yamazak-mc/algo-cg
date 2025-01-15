72a412383fdc6e99c48bcbac0838c2fb356328ae
Yamazak-mc <50317858+Yamazak-mc@users.noreply.github.com>
2025-01-15 19:51:20 +0900

    remove bevy window

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,97 @@
-#![allow(unused)]
-#![warn(unused_mut, unused_must_use)]
+// #![allow(unused)]
+// #![warn(unused_mut, unused_must_use)]
 
-use bevy::{input::common_conditions::input_just_pressed, prelude::*};
+use std::net::SocketAddr;
 
-const ADDR: &str = "0.0.0.0:54345";
+use log::{error, info};
+use protocol::WithMetadata;
+use tokio::{
+    io::AsyncWriteExt,
+    net::{TcpSocket, TcpStream},
+};
 
+type InboundEvent = protocol::client_to_server::ClientToServerEvent;
+type OutboundEvent = protocol::server_to_client::ServerToClientEvent;
+
+const ADDR: &str = "0.0.0.0";
 const DEFAULT_PORT: u16 = 54345;
 
-mod server;
-use server::ServerHandle;
+#[tokio::main]
+async fn main() {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .insert_resource(ServerHandle::start(ADDR))
-        .insert_resource(DummyResource::new())
-        .add_systems(
-            Update,
-            {
-                |mut server: ResMut<ServerHandle>| {
-                    if server.is_alive() {
-                        server.shutdown();
+    let socket = TcpSocket::new_v4().unwrap();
+    let addr = format!("{}:{}", ADDR, DEFAULT_PORT).parse().unwrap();
+    socket.bind(addr).unwrap();
+
+    info!("Server listening on port {}", addr.port());
+
+    let listener = socket.listen(1024).unwrap();
+
+    loop {
+        let (stream, socket_addr) = listener.accept().await.unwrap();
+        info!("connected to: {}", socket_addr);
+
+        tokio::spawn(async move {
+            Connection::new(stream, socket_addr).run().await;
+        });
+    }
+}
+
+struct Connection {
+    stream: TcpStream,
+    socket_addr: SocketAddr,
+}
+
+impl Connection {
+    fn new(stream: TcpStream, socket_addr: SocketAddr) -> Self {
+        Self {
+            stream,
+            socket_addr,
+        }
+    }
+
+    async fn run(&mut self) {
+        let stream = &mut self.stream;
+        let mut buf = vec![0; 1024];
+
+        loop {
+            let Ok(_) = stream.readable().await else {
+                break;
+            };
+
+            match stream.try_read(&mut buf) {
+                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    error!("read error: {}", e);
+                    break;
+                }
+                Ok(0) => {
+                    break;
+                }
+                Ok(n) => {
+                    // Received an event
+                    let data: WithMetadata<InboundEvent> =
+                        bincode::deserialize(&buf[0..n]).unwrap();
+                    info!("{:?}", data);
+
+                    match &data.event {
+                        InboundEvent::RequestJoin => {
+                            // TODO
+                            // For now, just accept all requests
+                            let resp = data.response_to(OutboundEvent::RequestJoinAccepted);
+                            let resp = bincode::serialize(&resp).unwrap();
+                            stream.write_all(&resp).await.unwrap();
+                        }
                     }
                 }
             }
-            .run_if(input_just_pressed(KeyCode::Escape)),
-        )
-        .run();
-}
+        }
 
-#[derive(Resource)]
-struct DummyResource;
-
-impl DummyResource {
-    fn new() -> Self {
-        info!("initialized DummyResource");
-        Self
+        info!("disconnected from: {}", self.socket_addr);
     }
 }
